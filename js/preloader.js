8ef76e760d02362e9364fbbe6616330f25083620
Miguel Angel <miguelhdezroa@outlook.com>
2020-11-06 17:17:52 -0500

    eight commit

---
+++ js/preloader.js
@@ -0,0 +1,6 @@
+$(window).on('load', function() { // makes sure the whole site is loaded 
+  $('#preloader').fadeOut(); // will first fade out the loading animation 
+ 
+  $('html').removeClass('hidden');
+  
+})
